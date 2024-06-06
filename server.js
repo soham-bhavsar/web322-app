98a69e070b496a153df36e6ec0d90f3307603ddb
zcheng18 <zcheng18@myseneca.ca>
2024-06-06 16:49:34 -0400

    link to github and vercel updated

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 /*********************************************************************************
 
 WEB322 – Assignment 02
-I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
 Name: Zexing Cheng
 Student ID: 162654214
 Date: 2024-06-06
-Cyclic Web App URL: _______________________________________________________
-GitHub Repository URL: ______________________________________________________
+Vercel Web App URL: https://vercel.com/zexing-chengs-projects/web322-app
+GitHub Repository URL: https://github.com/sc128307/web322-app
 
 ********************************************************************************/ 
 
