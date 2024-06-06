e52e227c85d9a3924a06903a5d1ea2fcd0e4e023
zcheng18 <zcheng18@myseneca.ca>
2024-06-06 15:23:35 -0400

    updates to server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: ______________________ 
+Student ID: ______________ 
+Date: ________________
+Cyclic Web App URL: _______________________________________________________
+GitHub Repository URL: ______________________________________________________
+
+********************************************************************************/ 
+
+
 // Import the express module
 const express = require('express');
 const path = require('path');
